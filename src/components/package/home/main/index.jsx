207e4ed8f8d21d542ff59cedb3a8aa3932ecffc1
jungjung261 <dungmv.now@gmail.com>
2023-04-28 12:57:00 +0700

    fix heading title

--- src/components/package/home/main/index.jsx
+++ src/components/package/home/main/index.jsx
@@ -24,7 +24,7 @@ const MainPage = (data) => {
                                 direction='left'
                                 cascade
                                 duration={100}>
-                                Wellcome to Jsound!
+                                Welcome to Jsound!
                             </Zoom>{' '}
                             <br />
                             <Zoom
